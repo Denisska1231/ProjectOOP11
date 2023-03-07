6a748d7887f075b744bddb793f22fb0bd65a5070
Yuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2023-03-08 01:30:47 +0700

    Add files via upload

---
+++ UI/components/Command.jsx
@@ -0,0 +1,32 @@
+
+import React from "react";
+import '../styles/button.css';
+
+export default function Command() {
+  return (
+    <div
+      className="d-flex justify-content-center  fw-bold h5 p-3 gap-4"
+      style={{ color: "#3C98E0" }}
+    >
+        <div>
+        your point 
+        
+        </div>
+        <button class="btn-1">
+        say hi
+        </button>
+        <button>
+        say hi
+        </button>
+        <button>
+        say hi
+        </button>
+        <button>
+        say hi
+        </button>
+        <button>
+        say hi
+        </button>
+    </div>
+  );
+}
