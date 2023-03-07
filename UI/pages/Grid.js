e6936e938ee77c03973d1972f045cd235535a5c7
Yuuki <90751280+YuuKiNaRak@users.noreply.github.com>
2023-03-08 01:27:15 +0700

    Update Grid.js

--- UI/pages/Grid.js
+++ UI/pages/Grid.js
@@ -1,18 +1,25 @@
 import React from "react";
-import HexagonGrid from "./hexagongrid.js";
-import times from "lodash/times";
+import HexagonGrid from "./HexagonGrid";
+// import Hexagon from "react-hexagon";
+import Hexagon from "./Hexagon";
 
-const HexGridDemo = () => {
+const x=5;
+const y=5;
+
+export const datamap = [];
+for (let loc = 0; loc < x*y; loc++) {
+  datamap[loc]='null';
+}
+const HexGrid = () => {
   const getHexProps = (hexagon) => {
     return {
       style: {
         fill: "#007aff",
         stroke: "white"
       },
-      onClick: () => alert(`Hexagon n.${hexagon} has been clicked`)
+      onClick: () => alert(`Hexagon number ${hexagon} `)
     };
   };
-
   const renderHexagonContent = (hexagon) => {
     return (
       <text
@@ -27,22 +34,34 @@ const HexGridDemo = () => {
       </text>
     );
   };
+  const hexagons = [];
+  let i=-1;
+  for (let row = 0; row < x; row++) {
+    if(i <= 1){i++}
+    for (let col = 0; col < y; col++) {
+      const hexagon = (
+        <Hexagon key={`${row}-${col}`} style={{ stroke: "white", fill: "#A1A1A1" }} flatTop>
 
-  let hexagons = times(20, (id) => id);
-  let x = 4;
-  let y = 5;
+        </Hexagon>
+      );
+      hexagons.push(i);
+      i++
+    }
+  }
 
   return (
-    <HexagonGrid
-      gridWidth={500}
-      gridHeight={500}
-      hexagons={hexagons}
-      rowsx={x}
-      columsy={y}
-      hexProps={getHexProps}
-      renderHexagonContent={renderHexagonContent}
-    />
+    <div>
+      <HexagonGrid
+        hexagons={hexagons}
+        gridWidth={500}
+        gridHeight={500}
+        rowsx={4}
+        columsy={5}
+        hexProps={getHexProps}
+        renderHexagonContent={renderHexagonContent}
+      />
+    </div>
   );
 };
 
-export default HexGridDemo;
+export default HexGrid;
